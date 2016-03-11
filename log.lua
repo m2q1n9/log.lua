3461b810e4bb3540afc5ceada5d9f3660b9118e0
m2q1n9 <m2q1n9@users.noreply.github.com>
2016-03-11 16:30:47 +0800

    update

--- log.lua
+++ log.lua
@@ -2,36 +2,36 @@
 
 local log = {}
 
-local _print = print
-
 local _format = function(...)
 	return select("#", ...) > 1 and string.format(...) or ...
 end
 
-function log.print(level, ...)
+local _print = print
+
+function _print1(level, ...)
 	_print(_format("%s %s/%s", os.date("%F %T"), level, _format(...)))
 end
 
-function log.print1(level, ...)
+function _print2(level, ...)
 	local info = debug.getinfo(3, "Sl")
 	local file, line = info.short_src, info.currentline
 	file = file:match(".*/(.*).lua") or file
-	log.print(level, "%s:%d %s", file, line, _format(...))
+	_print1(level, "%s:%d %s", file, line, _format(...))
 end
 
 function log.init(print, dev)
 	if print then _print = print end
+	print = dev and _print2 or _print1
 
 	if dev then
-		log.debug = function(...) log.print1("D", ...) end
+		log.debug = function(...) _print2("D", ...) end
 	else
 		log.debug = function(...) end
 	end
 
-	local log_print = dev and log.print1 or log.print
-	log.info  = function(...) log_print("I", ...) end
-	log.warn  = function(...) log_print("W", ...) end
-	log.error = function(...) log_print("E", ...) end
+	log.info  = function(...) print("I", ...) end
+	log.warn  = function(...) print("W", ...) end
+	log.error = function(...) print("E", ...) end
 
 	log.assert = function(v, ...) return assert(v, _format(...)) end
 	log.traceback = function(...) log.debug(debug.traceback(_format(...), 2)) end
